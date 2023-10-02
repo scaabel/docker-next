f144f617cab40960365a5f7d2138d63287a5a7e5
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-02 17:18:38 +0700

    Implement Main component

---
+++ src/components/main.tsx
@@ -0,0 +1,11 @@
+import Share from "@/components/share";
+
+const Main = () => {
+    return (
+        <div className="main-wrapper mt-4">
+            <Share />
+        </div>
+    )
+}
+
+export default Main
