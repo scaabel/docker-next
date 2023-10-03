23231fdb7f5a66fab2b76f6e8f9875fc6f253c6f
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-03 19:44:21 +0700

    Rework Header component with fully functional login and showing my account info

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,24 +1,53 @@
-const Login = () => {
+import {useState} from "react";
+
+const Login = ({setCurrUser, setIsAuth}: any) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const login = async () => {
+        const req = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        const reqValue = await req.json()
+
+        if (req.ok) {
+            setCurrUser(reqValue)
+            setIsAuth(true)
+        } else {
+            alert('Cannot login!')
+        }
+    }
+
     return (
         <div className="login-form-wrapper">
             <div className="flex-1 flex flex-col md:flex-row gap-4 md:gap-2">
                 <div className="w-full flex-1">
-                    <div className="bg-white flex border border-gray-200 rounded">
+                    <div className="bg-white h-full flex border border-gray-200 rounded">
                         <input placeholder="Email"
                                type="email"
+                               value={email}
+                               onChange={e => setEmail(e.target.value)}
                                className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/>
                     </div>
                 </div>
                 <div className="w-full flex-1">
-                    <div className="bg-white flex border border-gray-200 rounded">
+                    <div className="bg-white h-full flex border border-gray-200 rounded">
                         <input placeholder="Password"
                                type="password"
+                               value={password}
+                               onChange={e => setPassword(e.target.value)}
                                className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/>
                     </div>
                 </div>
-                <div className="w-full flex-1">
+                <div className="w-max-fit">
                     <button type="button"
-                            className="p-1 px-2 bg-blue-500 w-full text-gray-800 border border-gray-200 rounded text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            onClick={login}
+                            className="p-1 px-2 bg-blue-500 w-full text-gray-800 border border-gray-200 rounded text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center">
                         Login/Register
                     </button>
                 </div>
--- src/components/myAccount.tsx
+++ src/components/myAccount.tsx
@@ -1,18 +1,42 @@
-const MyAccount = () => {
-    return (
+const MyAccount = ({currUser, setIsAuth, setIsFormShare}: any) => {
+    const logout = async () => {
+        const req = await fetch('/api/logout', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: null
+        })
+
+        if (req.ok) {
+            // setCurrUser(null)
+            setIsAuth(false)
+        } else {
+            alert('Cannot logout!')
+        }
+    }
+
+    const shareFormToggle = () => {
+        setIsFormShare(true)
+    }
+
+    return (currUser &&
         <div className="my-account-wrapper">
             <div className="flex-1 flex flex-col md:flex-row gap-4 md:gap-2 items-center">
                 <div className="w-full flex-auto text-right">
-                    Welcome aa@aa.haha
+                    Welcome {currUser.email}
                 </div>
                 <div className="flex-auto">
                     <button type="button"
+                            onClick={shareFormToggle}
                             className="w-max">
                         Share a movie
                     </button>
                 </div>
                 <div className="flex-auto">
                     <button type="button"
+                            onClick={logout}
                             className="w-max">
                         Logout
                     </button>
