ce22218c3df8f2e3831d062ce4af3f97c26019b5
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-03 19:45:29 +0700

    Rework Index page to feed more props to children components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Header from "@/components/header";
 import Main from "@/components/main";
-import Share from "@/components/share";
+import {NextRequest, NextResponse} from "next/server";
+import {useState} from "react";
 
-export default function Home() {
+export default function Home({isLogin}: any) {
+    const [currUser, setCurrUser] = useState()
+    const [isAuth, setIsAuth] = useState(isLogin)
+    const [isFormShare, setIsFormShare] = useState(false)
 
 
     return (
         <main className="container mx-auto m-4">
-            <Header />
-            <Main />
+            <Header isAuth={isAuth} setIsAuth={setIsAuth} currUser={currUser} setCurrUser={setCurrUser}
+                    setIsFormShare={setIsFormShare}/>
+            <Main isFormShare={isFormShare} setIsFormShare={setIsFormShare} isAuth={isAuth} currUser={currUser}/>
         </main>
     )
+}
+
+export const getServerSideProps = async ({req, res}: { req: NextRequest, res: NextResponse }) => {
+    return {
+        props: {
+            isLogin: req.cookies.hasOwnProperty('login_token')
+        }, // Will be passed to the page component as props
+    }
 }
