0b298708df6bb4829f6079f91811e6507a49a53e
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-03 19:45:50 +0700

    Rework login API to make it work with Prisma

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import {NextApiRequest, NextApiResponse} from "next";
 import {PrismaClient} from '@prisma/client'
-import crypto from 'crypto'
 import {setCookie} from "cookies-next";
+import crypto from "crypto";
 
 const prisma = new PrismaClient()
 
@@ -14,37 +14,26 @@ export default async function handler(
         // Process a POST request
         const {email, password} = req.body
         if (email && password) {
-            //
             const result = await prisma.user.findFirst({
                 where: {
                     email: email,
-                },
-                include: {
-                    items: true
-                },
-            })
-
-            if (result) {
-                let hashPwd = crypto.createHash('md5').update(password).digest('hex');
-                if (result.password === hashPwd) {
-                    //login ok
-                    setCookie('login_token', hashPwd, {
-                        req,
-                        res,
-                        maxAge: 60 * 60 * 24,
-                        httpOnly: true,
-                        path: '/'
-                    })
-
-                    return res.status(201).json(result)
                 }
-                return res.status(403)
+            })
+            let hashPwd = crypto.createHash('md5').update(password).digest('hex');
+            if (result?.password === hashPwd) {
+                //login ok
+                setCookie('login_token', result, {
+                    req,
+                    res,
+                    maxAge: 60 * 60 * 24,
+                    httpOnly: true,
+                    path: '/'
+                })
+
+                return res.status(201).json(result)
             }
-        } else {
-            res.status(400)
+            return res.status(403).json({})
         }
-    } else {
-        // Handle any other HTTP method
-        res.status(400)
     }
+    return res.status(400).json({})
 }
